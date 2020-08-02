a071c4d6dffbfd27dc1d1734dc9cbbbc2dc99963
github username <github email address>
2020-08-02 19:12:49 -0400

    Set up initial pages and bootstrap

--- app/javascript/components/app.js
+++ app/javascript/components/app.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react'
+import MainHeader from '../components/MainHeader/MainHeader'
+import MainNav from '../components/MainNav/MainNav'
 import './app.css'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h2>StickMap: Chapman Stick fretboard visualization app</h2>
-        <hr />
-
-        home | settings | account
-
-        <hr />
-        <hr />
-        <hr />
-        <hr />
-      </div>
+      <>
+        <MainHeader />
+        <MainNav />
+      </>
     )
   }
 }

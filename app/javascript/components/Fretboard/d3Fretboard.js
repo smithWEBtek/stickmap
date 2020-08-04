e8363b0b6aaed33e13928b689418407242498b6b
github username <github email address>
2020-08-04 19:05:04 -0400

    Update fretboard

--- app/javascript/components/Fretboard/d3Fretboard.js
+++ app/javascript/components/Fretboard/d3Fretboard.js
@@ -27,23 +27,38 @@ window.addEventListener('DOMContentLoaded', () => {
 	const yAxisGroup = graph.append('g')
 
 	// fretboard data for 10 string railboard, standard tuning, with melody matching lowest bass string
-	const data = {
-		// frets: ['27', '26', '25', '24', '23', '22', '21', '20', '19', '18', '17', '16', '15', '14', '13', '12', '11', '10', '9', '8', '7', '6', '5', '4', '3', '2', '1'],
-		frets: [27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
-		notes: ['A', 'A#', 'Bb', 'B', 'B#', 'Cb', 'C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'E#', 'Fb', 'F', 'F#', 'Gb', 'G', 'G#', 'Ab'],
-		strings: [...Array(10).keys()]
-	}
+	// frets: ['27', '26', '25', '24', '23', '22', '21', '20', '19', '18', '17', '16', '15', '14', '13', '12', '11', '10', '9', '8', '7', '6', '5', '4', '3', '2', '1'],
+	const data = [
+		{
+			id: 1,
+			frets: [27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+			notes: ['A', 'A#', 'Bb', 'B', 'B#', 'Cb', 'C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'E#', 'Fb', 'F', 'F#', 'Gb', 'G', 'G#', 'Ab'],
+			strings: [...Array(10).keys()]
+		},
+		{
+			id: 2,
+			frets: [27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+			notes: ['A', 'A#', 'Bb', 'B', 'B#', 'Cb', 'C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'E#', 'Fb', 'F', 'F#', 'Gb', 'G', 'G#', 'Ab'],
+			strings: [...Array(10).keys()]
+		},
+		{
+			id: 3,
+			frets: [27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+			notes: ['A', 'A#', 'Bb', 'B', 'B#', 'Cb', 'C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'E#', 'Fb', 'F', 'F#', 'Gb', 'G', 'G#', 'Ab'],
+			strings: [...Array(10).keys()]
+		}
+	]
 
 	console.log('data: ', data)
 	console.log('graphHeight: ', graphHeight)
 	// set y domain using min/max of data.frets attribute
 	// set y range using graphHeight(calculated above) and 0 
 	const y = d3.scaleLinear()
-		.domain([0, d3.max(data.frets)])
+		.domain([0, d3.max(data[0].frets)])
 		.range([graphHeight, 0]);
 
 	const x = d3.scaleBand()
-		.domain(data.strings.map(item => item + 1)) // array of string #s for x domain
+		.domain(data[0].strings.map(item => item + 1)) // array of string #s for x domain
 		.range([0, graphWidth]) // range within overall graph width
 		.paddingInner(0.1)
 		.paddingOuter(0.2);

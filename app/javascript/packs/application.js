fc5d85ebcc5c276254b20edef54567fc0774a328
github username <github email address>
2020-08-09 12:33:04 -0400

    Add data, render first bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ require("d3")
 
 require("./app")
 require("../components/Fretboard/d3Fretboard")
+require('../components/fretboardData/fretboardData')
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
